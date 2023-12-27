0c2baaddfb710feed360521427e54035f08db272
Vaderiana <120415111+Vaderiana@users.noreply.github.com>
2023-12-27 20:58:06 +0300

    fix todos tests

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -2,8 +2,6 @@ import org.example.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TodosTest {
     @Test
     public void shouldAddThreeTasksOfDifferentType() {
@@ -32,7 +30,7 @@ class TodosTest {
     }
 
     @Test
-    public void SearchWhenFewTaskFound() {
+    public void shouldFindSeveralTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Купить Молоко");
 
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
@@ -57,7 +55,7 @@ class TodosTest {
     }
 
     @Test
-    public void SearchWhenZeroTaskFound() {
+    public void shouldFindOneTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Купить Молоко");
 
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
@@ -77,8 +75,32 @@ class TodosTest {
         todos.add(meeting);
 
         Task[] expected = {};
-        Task[] actual = todos.search("Написать");
+        Task[] actual = todos.search("вторник");
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void shouldFindNoTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Купить Молоко");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Несуществующая задача");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
